5a887be72c8ac918821dde96da57b7b1bb11a557
MajidovJavid <68659371+MajidovJavid@users.noreply.github.com>
2021-10-21 19:17:14 +0400

    Add files via upload

---
+++ hometask 21.10.2021/js/script.js
@@ -0,0 +1,54 @@
+// let height = +prompt("Shaxtababa deyir yolka neboyda olsun?");
+// herringbone(height);
+
+// function herringbone(height) {
+//     for ( let i = 0; i < height ; i++ ) {
+//         let star = ' *';
+//         let space = ' ';
+        
+
+//         for ( let j = 1; j <= i; j++ ) {
+//             star = star + '**';            
+//         }
+           
+//         let spaces = space.repeat(height-i-1);
+//         star = spaces + star;
+//         console.log(star);
+//     }
+// }
+
+
+// -----------------------------------------------------------------
+
+
+
+let arr = [32,25,85,89,45,76,7,41,2,12];
+// console.log(arr);
+
+// console.log(Math.max(...arr))
+// console.log(arr);
+
+// arr.push(15);
+// console.log(arr);
+
+// arr.shift();
+// console.log(arr);
+
+// arr.prototype.last = function() {
+//     return this[this.length - 1];
+//    }
+// arr.last();
+// console.log(arr); 
+
+// let popped = arr.pop();
+// console.log(popped);
+
+// arr.splice(4,0,"43");
+// console.log(arr);
+
+// arr.splice(0,3);
+// console.log(arr);
+
+// arr.forEach(element => console.log(element));
+
+
